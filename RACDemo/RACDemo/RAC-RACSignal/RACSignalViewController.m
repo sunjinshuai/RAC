3afb9c8d4e963f41286c11ffe358a4690a3804af
sunjinshuai <s_mike@163.com>
2018-05-28 17:30:19 +0800

    RAC-RACSignal

--- RACDemo/RACDemo/RAC-RACSignal/RACSignalViewController.m
+++ RACDemo/RACDemo/RAC-RACSignal/RACSignalViewController.m
@@ -48,4 +48,23 @@
     [disposable dispose];
 }
 
+/**
+ *
+ RACSignal总结：
+ 一.核心：
+ 1.核心：信号类
+ 2.信号类的作用：只要有数据改变就会把数据包装成信号传递出去
+ 3.只要有数据改变就会有信号发出
+ 4.数据发出，并不是信号类发出，信号类不能发送数据
+ 一.使用方法：
+ 1.创建信号
+ 2.订阅信号
+ 二.实现思路：
+ 1.当一个信号被订阅，创建订阅者，并把nextBlock保存到订阅者里面。
+ 2.创建的时候会返回 [RACDynamicSignal createSignal:didSubscribe];
+ 3.调用RACDynamicSignal的didSubscribe
+ 4.发送信号[subscriber sendNext:value];
+ 5.拿到订阅者的nextBlock调用
+ */
+
 @end
